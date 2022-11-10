b018b14a3e402ff5a1b2c7a5890a840e6b945841
Ismael <ismaelgutc@gmail.com>
2022-11-10 12:12:16 +0100

    empty base project

---
+++ src/main/java/es/ismagc/todoreport/TodoreportApplication.java
@@ -0,0 +1,13 @@
+package es.ismagc.todoreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ismagc/todoreport/TodoreportApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ismagc.todoreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
